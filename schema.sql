998897b7466a830f347caa140d4721d6f92e3ccb
Cam Crow <19ccrow99@gmail.com>
2024-01-06 21:31:21 -0600

    schema: second idea for db

--- schema.sql
+++ schema.sql
@@ -1,9 +1,8 @@
-create table users (
-  user_id int auto_increment primary key,
-  name text not null,
-  email text not null unique,
-  password text not null,
-  profile_image text
+create table profiles (
+  profile_id int not null primary key,
+  username text unique,
+  full_name text,
+  avatar_url text
 );
 
 create table tracks (
@@ -15,17 +14,17 @@ create table tracks (
 );
 
 create table playlists (
-  playlist_id int auto_increment primary key,
-  user_id int,
+  playlist_id int primary key,
+  profile_id int,
   name text not null,
   image text,
-  foreign key (user_id) references users(user_id)
+  foreign key (profile_id) references profiles(profile_id)
 );
 
 create table playlist_tracks (
   playlist_id int,
   track_id int,
-  order int,
+  rank int,
   primary key (playlist_id, track_id),
   foreign key (playlist_id) references playlists(playlist_id),
   foreign key (track_id) references tracks(track_id)
@@ -33,7 +32,7 @@ create table playlist_tracks (
 
 create table favorite_playlists (
   playlist_id int,
-  user_id,
+  profile_id int,
   foreign key (playlist_id) references playlists(playlist_id),
-  foreign key (user_id) references users(user_id)
+  foreign key (profile_id) references profiles(profile_id)
 );
